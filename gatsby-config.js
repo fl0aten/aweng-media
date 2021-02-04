493a90a6807ad7e275a1c5f80f5fb02e53f3ab55
Dominic Holzinger <d.holzinger@aweng.media>
2021-02-04 06:21:48 +0100

    replaced bootstrap with tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,16 @@
 module.exports = {
-  siteMetadata: {
-    title: "bla",
-  },
-  plugins: ["gatsby-plugin-sass"],
+    siteMetadata: {
+        title: "bla",
+    },
+    plugins: [
+        {
+            resolve: `gatsby-plugin-sass`,
+            options: {
+                postCssPlugins: [
+                    require("tailwindcss"),
+                    require("./tailwind.config.js"), // Optional: Load custom Tailwind CSS configuration
+                ],
+            },
+        },
+    ],
 }
