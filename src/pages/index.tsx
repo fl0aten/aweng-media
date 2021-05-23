1cb1fa7e054ddc62ee0d9273a8dbd864c07d9035
Dominic Holzinger <dome.holzinger@gmail.com>
2021-05-23 18:47:57 +0200

    removed old content and added new navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,137 +1,100 @@
-import React from "react";
-import { version } from "/package.json"
+import React, { Fragment } from "react";
+import { Popover, Transition } from '@headlessui/react'
+import * as logoDark from "../images/aweng-media-dark.svg";
 
 export default function Home() {
     return <main>
-        <nav className="py-5">
-            <div className="container">
-                <div>
-                    <a href="/" title="">&lt;awen<span style={{"font-variant": "small-caps"}}>g</span> me<span style={{"font-variant": "small-caps"}}>d</span>ia/&gt;</a>
-                </div>
-            </div>
-        </nav>
-        <section className="bg-gradient-to-r from-green-500 to-green-600 h-almost flex justify-center items-end">
-            <div className="-mb-16 w-full h-4/5 md:h-100 bg-hero bg-no-repeat bg-bottom bg-cover md:bg-auto" />
-        </section>
-        <section className="my-40">
-            <div className="container grid grid-cols-6 gap-4">
-                <div className="md:col-span-3 lg:col-start-2 col-span-6 lg:col-span-2 flex md:flex-col justify-center">
-                    <img className="w-4/5 md:w-full mb-8 md:mb-0" src="https://placehold.it/450x650?text=:)" alt=""/>
-                </div>
-                <div className="md:col-span-3 col-span-6 lg:col-span-3 xl:col-span-2 flex flex-col justify-center">
-                    <h2 className="text-6xl lg:text-7xl xl:text-8xl mb-10">Dominic Holzinger</h2>
-                    <p className="text-lg">
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-                        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-                        et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-                        Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                        diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-                        voluptua.
-                    </p>
-                </div>
-            </div>
-        </section>
-        <section className="relative bg-green-600 bg-opacity-50 pt-40 pb-60 z-10">
-            <div className="container grid grid-cols-10 text-white">
-                <div className="grid grid-cols-3 gap-4 sm:col-start-2 md:col-start-3 lg:col-start-1 xl:col-start-2 col-span-10 sm:col-span-8 md:col-span-6 lg:col-span-10 xl:col-span-8">
-                    <div className="col-span-3 lg:col-span-1">
-                        <h2 className="text-2xl uppercase text-center bg-green-900 py-4 mb-4">Lorem ipsum</h2>
-                        <p className="px-8 text-lg">
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
-                            ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
-                            dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor
-                            sit amet.
-                        </p>
-                    </div>
-                    <div className="col-span-3 lg:col-span-1">
-                        <h2 className="text-2xl uppercase text-center bg-green-900 py-4 mb-4">Lorem ipsum</h2>
-                        <p className="px-8 text-lg">
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
-                            ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
-                            dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor
-                            sit amet.
-                        </p>
-                    </div>
-                    <div className="col-span-3 lg:col-span-1">
-                        <h2 className="text-2xl uppercase text-center bg-green-900 py-4 mb-4">Lorem ipsum</h2>
-                        <p className="px-8 text-lg">
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
-                            ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
-                            dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor
-                            sit amet.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section className="relative z-0 mt-20 mb-40">
-            <div className="container grid grid-cols-10">
-                <div className="xl:col-start-2 col-span-10 xl:col-span-8">
-                    <div className="grid grid-cols-12 mb-8">
-                        <div className="md:col-start-2 col-span-12 md:col-span-2 flex items-end order-2">
-                            <h2 className="uppercase text-5xl leading-tight">
-                                Ein<br/>
-                                hauch<br/>
-                                nerdiger?
-                            </h2>
-                        </div>
-                        <div className="col-start-3 sm:col-start-5 md:col-start-6 lg:col-start-8 col-span-10 sm:col-span-8 md:col-span-6 lg:col-span-3 flex justify-center order-1">
-                            <img className="lg:-mt-60 mb-20 lg:my-auto md:mb-20" src="https://placehold.it/450x650?text=:)" alt=""/>
+        <Popover className="relative bg-gray-800">
+            {({ open }) => (
+                <>
+                    <div className="max-w-7xl mx-auto px-4 sm:px-6">
+                        <div className="flex justify-between items-center py-6 md:justify-start md:space-x-10">
+                            <div className="flex justify-start lg:w-0 lg:flex-1">
+                                <a href="#">
+                                    <span className="sr-only">aweng.media</span>
+                                    <img className="h-6 w-auto" src={ logoDark } alt=""/>
+                                </a>
+                            </div>
+                            <div className="-mr-2 -my-2 md:hidden">
+                                <Popover.Button className="p-2 inline-flex items-center justify-center rounded text-white hover:text-white hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                    <span className="sr-only">Menü öffnen</span>
+                                    <span className="material-icons">menu</span>
+                                </Popover.Button>
+                            </div>
+                            <Popover.Group as="nav" className="hidden md:flex space-x-10 flex items-center">
+                                <a href="#" className="text-base font-medium text-white hover:text-blue-500 font-montserrat">
+                                    Über mich
+                                </a>
+                                <a href="#" className="text-base font-medium text-white hover:text-blue-500 font-montserrat">
+                                    Dienstleistungen
+                                </a>
+                                <a href="#" className="whitespace-nowrap inline-flex uppercase border-blue-500 font-montserrat items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-blue-500 bg-transparent hover:text-white hover:bg-blue-500">
+                                    Kontakt
+                                </a>
+                                <a href="#" className="whitespace-nowrap text-base font-medium text-white font-montserrat hover:text-blue-500 ">
+                                    <span className="material-icons text-2xl">light_mode</span>
+                                </a>
+                            </Popover.Group>
                         </div>
                     </div>
-                    <p className="border-8 border-red-900 p-8 text-lg">
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-                        labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-                        et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-                        labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-                        et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                    </p>
-                </div>
-            </div>
-        </section>
-        <footer>
-            <section className="py-20 bg-gray-600 text-white">
-                <div className="container grid grid-cols-10 text-white">
-                    <div className="grid grid-cols-3 gap-4 sm:col-start-2 md:col-start-3 lg:col-start-1 xl:col-start-2 col-span-10 sm:col-span-8 md:col-span-6 lg:col-span-10 xl:col-span-8">
-                        <div className="col-span-3 lg:col-span-1">
-                            <h3 className="text-3xl mb-4">aweng media</h3>
-                            <address className="px-8 lg:px-0">
-                                Dominic Holzinger<br />
-                                Bohlleitenweg 31<br />
-                                97082 Würzburg
-                            </address>
-                        </div>
-                        <div className="col-span-3 lg:col-span-1">
-                            <h3 className="text-3xl mb-4 lowercase">Kontakt</h3>
-                            <address className="px-8 lg:px-0">
-                                tel <a href="tel:+4993132942414" className="underline hover:text-green-300">0931 32942414</a><br/>
-                                fax <a href="tel:+4993132919574" className="underline hover:text-green-300">0931 32919574</a><br/>
-                                mail <a href="mailto:info@aweng.media" className="underline hover:text-green-300">info@aweng.media</a>
-                            </address>
-                        </div>
-                        <div className="col-span-3 lg:col-span-1">
-                            <h3 className="text-3xl mb-4 lowercase">Social</h3>
-                            <p className="px-8 lg:px-0">
-                                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-                                labore et dolore magna aliquyam erat, sed diam voluptua.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section className="py-5 bg-gray-800 text-white">
-                <div className="container text-white grid grid-cols-10">
-                    <div className="grid grid-cols-2 xl:col-start-2 col-span-10 xl:col-span-8 gap-4">
-                        <div className="col-span-2 sm:col-span-1 text-center sm:text-left lowercase">
-                            Made with ❤️ by <a href="https://aweng.media" className="underline hover:text-green-300">Me</a>
-                        </div>
-                        <div className="col-span-2 sm:col-span-1 text-center sm:text-right lowercase">
-                            <a href={ "https://github.com/fl0aten/aweng-media/tree/" + version } target="_blank" className="underline hover:text-green-300">View on GitHub</a> | v{ version }
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </footer>
+
+                    <Transition
+                        show={ open }
+                        as={ Fragment }
+                        enter="duration-200 ease-out"
+                        enterFrom="opacity-0 scale-95"
+                        enterTo="opacity-100 scale-100"
+                        leave="duration-100 ease-in"
+                        leaveFrom="opacity-100 scale-100"
+                        leaveTo="opacity-0 scale-95"
+                    >
+                        <Popover.Panel focus static className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+                            <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-gray-800 divide-y-2 divide-gray-700">
+                                <div className="pt-5 pb-6 px-5">
+                                    <div className="flex items-center justify-between">
+                                        <div>
+                                            <img className="h-6 w-auto" src={ logoDark } alt="Workflow"/>
+                                        </div>
+                                        <div className="-mr-2">
+                                            <Popover.Button className="bg-transparent rounded-md p-2 inline-flex items-center justify-center text-white hover:text-white hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                                <span className="sr-only">Close menu</span>
+                                                <span className="material-icons">close</span>
+                                            </Popover.Button>
+                                        </div>
+                                    </div>
+                                    <div className="mt-6">
+                                        <nav className="grid gap-y-8 text-white">
+                                            <a href="#" className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-700">
+                                                <span className="material-icons text-blue-500">emoji_emotions</span>
+                                                <span className="ml-3 text-base font-medium">Über mich</span>
+                                            </a>
+                                            <a href="#" className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-700">
+                                                <span className="material-icons">list_alt</span>
+                                                <span className="ml-3 text-base font-medium">Dienstleistungen</span>
+                                            </a>
+                                            <a href="#" className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-700">
+                                                <span className="material-icons">light_mode</span>
+                                                <span className="ml-3 text-base font-medium">Licht an!</span>
+                                            </a>
+                                            <a href="#" className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-700">
+                                                <span className="material-icons">dark_mode</span>
+                                                <span className="ml-3 text-base font-medium">Licht aus!</span>
+                                            </a>
+                                        </nav>
+                                    </div>
+                                </div>
+                                <div className="py-6 px-5 space-y-6">
+                                    <div>
+                                        <a href="#" className="uppercase w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-500 hover:bg-indigo-700">
+                                            Kontakt
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </Popover.Panel>
+                    </Transition>
+                </>
+            )}
+        </Popover>
     </main>
 }
